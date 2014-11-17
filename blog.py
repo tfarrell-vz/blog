0ea27aa02b99e43ac82941b534046b7683291469
tfarrell-vz <farrell@abiresearch.com>
2014-11-16 19:41:37 -0500

    Blog post form structure.

--- blog.py
+++ blog.py
@@ -1,26 +1,45 @@
 import os
-
 import jinja2
 import webapp2
 
+from google.appengine.ext import db
+
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 
-jinja_env = jinja2.Environment(loader= jinja2.FileSystemLoader('template_dir'),
+jinja_env = jinja2.Environment(loader= jinja2.FileSystemLoader(template_dir),
                                autoescape=True)
 
 class Handler(webapp2.RequestHandler):
     def write(self, *args, **kwargs):
-        self.response.write(*args, **kwargs)
+        self.response.out.write(*args, **kwargs)
 
     def render_to_string(self, template, **kwargs):
         t = jinja_env.get_template(template)
         return t.render(**kwargs)
 
     def render(self, template, **kwargs):
-        return self.write(self.render_to_string(template, **kwargs))
+        self.write(self.render_to_string(template, **kwargs))
+
+class Post(db.Model):
+    subject = db.StringProperty()
+    content = db.TextProperty()
+    created = db.DateTimeProperty(auto_now_add=True)
 
 class BlogHandler(Handler):
     def get(self):
-        self.response.out.write("Hello, world!")
+        self.render('index.html')
 
-app = webapp2.WSGIApplication([('/blog', BlogHandler)], debug=True)
+
+class NewPostHandler(Handler):
+    def get(self):
+        self.render('newpost.html')
+
+    def post(self):
+        subject = self.request.get('subject')
+        content = self.request.get('content')
+
+
+
+app = webapp2.WSGIApplication([('/blog', BlogHandler),
+                               ('/blog/newpost', NewPostHandler),
+                               ], debug=True)
